e309793005597f58c7abcc200d48bf378b4ce362
suki-9 <suki.music2004@gmail.com>
2024-02-22 23:38:57 +0900

    fix read body

--- package/backend/controllers/api.cc
+++ package/backend/controllers/api.cc
@@ -5,6 +5,7 @@
 
 /* ----- use modules include -----*/
 #include "../modules/logger_module.h"
+#include "json_module.h"
 #include "game_core_module.h"
 #include "db_module.h"
 
@@ -12,36 +13,45 @@
 void api::post_create(
   const HttpRequestPtr &req, std::function<void(const HttpResponsePtr &)> &&callback
 ) {
-  std::string pass = req->getParameter("pass");
-  Json::Value json;
+  Json::Value json_res;
+  Json::Value json_req;
+  json_parse(json_req, std::string(req->body()));
+  std::string pass = json_req["pass"].asCString();
 
-  std::cout << pass << std::endl;
+  bool is_err = false;
 
   if (verify_pass(pass)) {
     PlayerId playerId = create_id();
     InstanceId instance_id = create_instance(pass, playerId);
+
     if (instance_id != "") {
-      json["roomId"] = instance_id;
-      json["playerId"] = playerId;
+      json_res["roomId"] = instance_id;
+      json_res["playerId"] = playerId;
     } else {
-      json["err"] = "";
-      ERR_LOGGER( "API service", "Err -> " << json["err"] );
+      is_err = true;
+      json_res["err"] = "";
+      ERR_LOGGER( "API service", "Err -> " << json_res["err"] );
     }
   } else {
-    json["err"] = "Invalid pass error!";
-    ERR_LOGGER( "API service", "Err -> " << json["err"] );
+    is_err = true;
+    json_res["err"] = "Invalid pass error!";
+    ERR_LOGGER( "API service", "Err -> " << json_res["err"] );
   }
 
-  auto res = HttpResponse::newHttpJsonResponse(json);
-  res->setStatusCode(k200OK);
+  auto res = HttpResponse::newHttpJsonResponse(json_res);
+  res->setStatusCode(is_err ? k500InternalServerError : k200OK);
   callback(res);
 }
 
 void api::post_find(
   const HttpRequestPtr &req, std::function<void(const HttpResponsePtr &)> &&callback
 ) {
-  std::string pass = req->getParameter("pass");
-  Json::Value json;
+  Json::Value json_res;
+  Json::Value json_req;
+  json_parse(json_req, std::string(req->body()));
+  std::string pass = json_req["pass"].asCString();
+
+  bool is_err = false;
 
   std::string p[2]{""};
 
@@ -54,8 +64,8 @@ void api::post_find(
 
   if (instance.instance_id != "" && !(instance.is_matched)) {
     instance.player_ids[1] = create_id();
-    json["playerId"] = instance.player_ids[1];
-    json["roomId"] = instance.instance_id;
+    json_res["playerId"] = instance.player_ids[1];
+    json_res["roomId"] = instance.instance_id;
 
     DB_RUN_WITH_VALUE(
       DB_UPDATE_QUERY("pass = '" + pass + "'", "player_ids", "is_matched"),
@@ -63,11 +73,12 @@ void api::post_find(
     );
 
   } else {
-    json["err"] = "Your request pass is Not found";
-    ERR_LOGGER( "API service", "Err -> " << json["err"] );
+    is_err = true;
+    json_res["err"] = "Your request pass is Not found";
+    ERR_LOGGER( "API service", "Err -> " << json_res["err"] );
   }
 
-  auto resp = HttpResponse::newHttpJsonResponse(json);
-  resp->setStatusCode(k200OK);
-  callback(resp);
+  auto res = HttpResponse::newHttpJsonResponse(json_res);
+  res->setStatusCode(is_err ? k500InternalServerError : k200OK);
+  callback(res);
 }
