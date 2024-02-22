f18a5b364ab9f060d5838655810a78d51b02a386
suki-9 <suki.music2004@gmail.com>
2024-02-22 12:05:16 +0900

    change query

--- table.sql
+++ table.sql
@@ -1,8 +1,6 @@
 CREATE TABLE IF NOT EXISTS game_instance (
   create_at      BIGINT       NOT NULL,
-  current_player INTEGER      default 1,
   player_ids     TEXT[2]      NOT NULL,
-  board          INTEGER[64]  NOT NULL,
   pass           TEXT         NOT NULL,
   instance_id    TEXT         NOT NULL,
   is_matched     BOOLEAN      default false, 
