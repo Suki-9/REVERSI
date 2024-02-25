5ca3f4c3259fa60042adc285f0c4f3b00a4b8cf0
suki-9 <suki.music2004@gmail.com>
2024-02-25 20:24:39 +0900

    なんかもう色々追加

--- package/backend/modules/logger_module.h
+++ package/backend/modules/logger_module.h
@@ -1,5 +1,12 @@
 #pragma once
 
 /* ----- LOGGER macros declaration -----*/
-#define ERR_LOGGER( service, text ) std::cout << "[" << service << " service - " << __func__ << "]: " << text << std::endl
-#define LOGGER( service, text ) std::cout << "[" << service << " service - " << __func__ << "]: " << text << std::endl
+#define ERR_LOGGER( service, text ) std::cout << red << "ERR [" << service << " Service - " << __func__ << "]: " << reset << text << std::endl
+#define WARN_LOGGER( service, text ) std::cout << yellow << "WARN [" << service << " Service - " << __func__ << "]: " << reset << text << std::endl
+#define LOGGER( service, text ) std::cout << blue << "INFO [" << service << " Service - " << __func__ << "]: " << reset << text << std::endl
+
+#define red "\e[31m"
+#define green "\e[32m"
+#define blue "\e[34m"
+#define yellow "\e[33m" 
+#define reset "\e[0m"
